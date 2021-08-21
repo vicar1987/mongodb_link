0b33f8881004bb2c6e6a3020425da0189eb8984e
vicar1987 <mithrandirvic@gmail.com>
2021-08-21 23:22:00 +0800

    edit mongodb link info

--- mongodb.py
+++ mongodb.py
@@ -1,7 +1,7 @@
 import pymongo
 # from pymongo import MongoClient
 
-client = pymongo.MongoClient("mongodb+srv://vicar1987:1ul3u03nji3@twfruit.i2omj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://<username>:<password>@<project_name>.i2omj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.TWFruits
 test = db.test
 
